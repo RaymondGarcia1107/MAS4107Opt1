995dd86d22861a924217d5632810df8353fa8a25
Ramson Munoz <rmuno@protonmail.com>
2024-10-26 19:13:01 -0400

    Fixed method to create B matrix appropriately using sparse framework. Tested with a local main to ensure appropriate output

--- HeatModel.py
+++ HeatModel.py
@@ -8,14 +8,16 @@ Created on Sat Oct 26 10:28:29 2024
 
 from scipy import sparse
 import numpy as np
+import sys
 
 class HeatModel:
     
 
     def __init__(self,n):
         self.n = n
-        
-    def create_large_diagonal_matrix(self)->np.ndarray:
+    
+    
+    def create_large_diagonal_matrix(self):
         """
         This function generates B from the assignment document
 
@@ -26,16 +28,47 @@ class HeatModel:
 
         Returns
         -------
-        n x n Matrix of ones where diagonals have -4.
+        n x n Sparse Matrix of ones where diagonals have -4 on main positive diagonal and 1 on offset diagonals.
         
         ex 3 x 3
         
-        [[-4 1 1]
+        [[-4 1 0]
          [1 -4 1]
-         [1 1 -4]]
+         [0 1 -4]]
+        
+        ex 4 x 4
+        
+        [[-4.  1.  0.  0.]
+         [ 1. -4.  1.  0.]
+         [ 0.  1. -4.  1.]
+         [ 0.  0.  1. -4.]]
+        
+        
+        ex 10 x 10
+        
+        [[-4.  1.  0.  0.  0.  0.  0.  0.  0.]
+         [ 1. -4.  1.  0.  0.  0.  0.  0.  0.]
+         [ 0.  1. -4.  1.  0.  0.  0.  0.  0.]
+         [ 0.  0.  1. -4.  1.  0.  0.  0.  0.]
+         [ 0.  0.  0.  1. -4.  1.  0.  0.  0.]
+         [ 0.  0.  0.  0.  1. -4.  1.  0.  0.]
+         [ 0.  0.  0.  0.  0.  1. -4.  1.  0.]
+         [ 0.  0.  0.  0.  0.  0.  1. -4.  1.]
+         [ 0.  0.  0.  0.  0.  0.  0.  1. -4.]]
 
         """
+        try:
+            
+            if(self.n < 2):
+                raise ValueError("Ensure model size is at least 2")
         
-        return np.ones((self.n,self.n)) - 5*np.eye(self.n)
+            data =[np.ones(self.n - 1),-4*np.ones(self.n),np.ones(self.n - 1)]
+            offsets = [-1,0,1]
+        
+            return sparse.diags(data,offsets,shape=(self.n,self.n))
+        
+        except ValueError as e:
+            print(e)
+            sys.exit()
         
         
