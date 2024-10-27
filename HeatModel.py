e49373c9bef88d68649227d9ff323602094143bd
Ramson Munoz <rmuno@protonmail.com>
2024-10-26 21:54:08 -0400

    finshed a potential solution for matrix generation. Cleaned up some of the comments and descriptions. Added Exceptions for constructor inputs

--- HeatModel.py
+++ HeatModel.py
@@ -13,62 +13,119 @@ import sys
 class HeatModel:
     
 
-    def __init__(self,n):
-        self.n = n
+    def __init__(self,size):
+        self.size = size
+        
+        self.designMatrix = self.create_Model()
     
     
-    def create_large_diagonal_matrix(self):
+    def create_diagonal_matrix(self,diagonalElement,offDiagonalElement):
         """
-        This function generates B from the assignment document
+        
 
         Parameters
         ----------
-        int : n
-            Size of matrix tile.
+        diagonalElement : float
+            main diagonal element for sparse diagonal element.
+        offDiagonalElement : float
+            general off diagonal element. our case uses ones.
+
+        Raises
+        ------
+        ValueError
+            returns error if constructor is called with non-positive int.
+        TypeError
+            if constructor input is not int
 
         Returns
         -------
-        n x n Sparse Matrix of ones where diagonals have -4 on main positive diagonal and 1 on offset diagonals.
-        
-        ex 3 x 3
-        
-        [[-4 1 0]
-         [1 -4 1]
-         [0 1 -4]]
-        
-        ex 4 x 4
-        
-        [[-4.  1.  0.  0.]
-         [ 1. -4.  1.  0.]
-         [ 0.  1. -4.  1.]
-         [ 0.  0.  1. -4.]]
-        
-        
-        ex 10 x 10
-        
-        [[-4.  1.  0.  0.  0.  0.  0.  0.  0.]
-         [ 1. -4.  1.  0.  0.  0.  0.  0.  0.]
-         [ 0.  1. -4.  1.  0.  0.  0.  0.  0.]
-         [ 0.  0.  1. -4.  1.  0.  0.  0.  0.]
-         [ 0.  0.  0.  1. -4.  1.  0.  0.  0.]
-         [ 0.  0.  0.  0.  1. -4.  1.  0.  0.]
-         [ 0.  0.  0.  0.  0.  1. -4.  1.  0.]
-         [ 0.  0.  0.  0.  0.  0.  1. -4.  1.]
-         [ 0.  0.  0.  0.  0.  0.  0.  1. -4.]]
+        TYPE
+            DESCRIPTION.
 
         """
+
         try:
             
-            if(self.n < 2):
-                raise ValueError("Ensure model size is at least 2")
+            int(self.size)
+            
+            if(self.size < 1):
+                raise ValueError("Ensure model size is at least 1")
+            elif(self.size == 1):
+                return np.array([[-4]])
         
-            data =[np.ones(self.n - 1),-4*np.ones(self.n),np.ones(self.n - 1)]
+            data =[offDiagonalElement * np.ones(self.size - 1),diagonalElement*np.ones(self.size),offDiagonalElement * np.ones(self.size - 1)]
             offsets = [-1,0,1]
         
-            return sparse.diags(data,offsets,shape=(self.n,self.n))
+            return sparse.diags(data,offsets,shape=(self.size,self.size))
         
         except ValueError as e:
             print(e)
             sys.exit()
+        except TypeError as e:
+            print(e)
+            sys.exit()
+            
+    def create_large_diagonal_matrix(self):
+        """
+        Generates the B matrix for our large design matrix
+
+        Returns
+        -------
+        Sparse.diag matrix
+
+        """
+        return self.create_diagonal_matrix(-4,1)
+    
+    
+
+            
+    def create_Model(self):
+        
+        """
+        
+
+        Returns
+        -------
+        scipy sparse matrix object
+            our design matrix as a scipy.sparse object.
+            
+            implemented as a block diagonal matrix such that main diagonal is an n x n matrix of the from B
+            
+            where B =[[-4.,  1.,  0.],
+                      [ 1., -4.,  1.],
+                      [ 0.,  1., -4.]] for 3 x 3
+            
+                  [[-4.,  1.,  0.,  0.,  0.,  0.,  0.],
+                   [ 1., -4.,  1.,  0.,  0.,  0.,  0.],
+                   [ 0.,  1., -4.,  1.,  0.,  0.,  0.],
+                   [ 0.,  0.,  1., -4.,  1.,  0.,  0.],
+                   [ 0.,  0.,  0.,  1., -4.,  1.,  0.],
+                   [ 0.,  0.,  0.,  0.,  1., -4.,  1.],
+                   [ 0.,  0.,  0.,  0.,  0.,  1., -4.]] for B(7 x 7)
+                  
+        generalized to nxn for positive values n
+        
+        and on the of diagonal blocks an Identity matrix of size n
+        
+        
+        final matrix is n^2 x n^2
+            
+            
+
+        """
+        
+        diagonalMatrix = self.create_large_diagonal_matrix()
+        
+        finalMatrix = sparse.block_diag([diagonalMatrix] * self.size)
+        
+        #creates Identity above main diagonal
+        finalMatrix.setdiag(np.ones((self.size * self.size) - self.size),self.size)
+        
+        #creates Identity below main diagonal
+        finalMatrix.setdiag(np.ones((self.size * self.size) - self.size),-self.size)
+        
+        return finalMatrix
+        
+        
         
         
