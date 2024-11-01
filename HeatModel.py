adb55bf6c8f84310d4a54d77394ddfadb181b4fb
Ramson Munoz <rmuno@protonmail.com>
2024-11-01 09:43:31 -0400

    finished up todos from previous commit.

--- HeatModel.py
+++ HeatModel.py
@@ -10,18 +10,17 @@ from scipy import sparse
 import numpy as np
 import sys
 
-class HeatModel:
-    
 
-    def __init__(self,size):
+class HeatModel:
+
+    def __init__(self, size):
         self.size = size
-        
+
         self.designMatrix = self.create_Model()
-    
-    
-    def create_diagonal_matrix(self,diagonalElement,offDiagonalElement):
+
+    def create_diagonal_matrix(self, diagonalElement, offDiagonalElement):
         """
-        
+
 
         Parameters
         ----------
@@ -39,32 +38,33 @@ class HeatModel:
 
         Returns
         -------
-        TYPE
-            DESCRIPTION.
+        sparse.diagonal object
+            a sparse matrix containing diagonal elements of the input element and off-diagonal elements as specified
 
         """
 
         try:
-            
+
             int(self.size)
-            
-            if(self.size < 1):
+
+            if (self.size < 1):
                 raise ValueError("Ensure model size is at least 1")
-            elif(self.size == 1):
+            elif (self.size == 1):
                 return np.array([[-4]])
-        
-            data =[offDiagonalElement * np.ones(self.size - 1),diagonalElement*np.ones(self.size),offDiagonalElement * np.ones(self.size - 1)]
-            offsets = [-1,0,1]
-        
-            return sparse.diags(data,offsets,shape=(self.size,self.size))
-        
+
+            data = [offDiagonalElement * np.ones(self.size - 1), diagonalElement*np.ones(
+                self.size), offDiagonalElement * np.ones(self.size - 1)]
+            offsets = [-1, 0, 1]
+
+            return sparse.diags(data, offsets, shape=(self.size, self.size))
+
         except ValueError as e:
             print(e)
             sys.exit()
         except TypeError as e:
             print(e)
             sys.exit()
-            
+
     def create_large_diagonal_matrix(self):
         """
         Generates the B matrix for our large design matrix
@@ -74,27 +74,23 @@ class HeatModel:
         Sparse.diag matrix
 
         """
-        return self.create_diagonal_matrix(-4,1)
-    
-    
+        return self.create_diagonal_matrix(-4, 1)
 
-            
     def create_Model(self):
-        
         """
-        
+
 
         Returns
         -------
         scipy sparse matrix object
             our design matrix as a scipy.sparse object.
-            
+
             implemented as a block diagonal matrix such that main diagonal is an n x n matrix of the from B
-            
+
             where B =[[-4.,  1.,  0.],
                       [ 1., -4.,  1.],
                       [ 0.,  1., -4.]] for 3 x 3
-            
+
                   [[-4.,  1.,  0.,  0.,  0.,  0.,  0.],
                    [ 1., -4.,  1.,  0.,  0.,  0.,  0.],
                    [ 0.,  1., -4.,  1.,  0.,  0.,  0.],
@@ -102,30 +98,28 @@ class HeatModel:
                    [ 0.,  0.,  0.,  1., -4.,  1.,  0.],
                    [ 0.,  0.,  0.,  0.,  1., -4.,  1.],
                    [ 0.,  0.,  0.,  0.,  0.,  1., -4.]] for B(7 x 7)
-                  
+
         generalized to nxn for positive values n
-        
+
         and on the of diagonal blocks an Identity matrix of size n
-        
-        
+
+
         final matrix is n^2 x n^2
-            
-            
+
+
 
         """
-        
+
         diagonalMatrix = self.create_large_diagonal_matrix()
-        
+
         finalMatrix = sparse.block_diag([diagonalMatrix] * self.size)
-        
-        #creates Identity above main diagonal
-        finalMatrix.setdiag(np.ones((self.size * self.size) - self.size),self.size)
-        
-        #creates Identity below main diagonal
-        finalMatrix.setdiag(np.ones((self.size * self.size) - self.size),-self.size)
-        
+
+        # creates Identity above main diagonal
+        finalMatrix.setdiag(
+            np.ones((self.size * self.size) - self.size), self.size)
+
+        # creates Identity below main diagonal
+        finalMatrix.setdiag(
+            np.ones((self.size * self.size) - self.size), -self.size)
+
         return finalMatrix
-        
-        
-        
-        
